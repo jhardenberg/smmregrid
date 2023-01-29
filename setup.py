29d92061e56e74983923a11f08f7ab66c8a2dbdd
Jost von Hardenberg <jost.hardenberg@polito.it>
2023-01-29 11:18:23 +0100

    added package env and setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+
+from distutils.core import setup
+
+setup(name='smmregrid',
+      version='0.0.1',
+      description='Regridding based on sparse matrix multiplication',
+      author=' Jost von Hardenberg, Scott Wales',
+      author_email='jost.hardenberg@polito.it',
+      url='https://github.com/jhardenberg/smmregrid',
+      python_requires='>3.7, <3.10',
+      packages=['smmregrid'],
+      install_requires=[
+          'numpy',
+          'xarray',
+          'netcdf4',
+          'dask',
+          'sparse',
+          'cfunits'
+      ]
+      )
