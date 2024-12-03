0c8e1659bc145185646eaa8b39c51b66d68648de
oloapinivad <p.davini@isac.cnr.it>
2024-12-03 16:16:14 +0100

    fix

--- tests/multigrid_test.py
+++ tests/multigrid_test.py
@@ -16,8 +16,8 @@ def test_multi_grid_inspection():
     data = xr.open_dataset(ifile)
     regridded = regrid.regrid(data)
     assert len(regrid.grids) == 2
-    assert vars(regrid.grids[1])['dims'] == ('time', 'lev', 'j', 'i')
-    assert vars(regrid.grids[0])['dims'] == ('time', 'plev', 'lat', 'lon')
+    assert vars(regrid.grids[1])['dims'] == ('lev', 'j', 'i')
+    assert vars(regrid.grids[0])['dims'] == ('plev', 'lat', 'lon')
     assert regridded['ua'].shape == (2, 3, 90, 180)
     assert regridded['so'].shape == (2, 3, 90, 180)
 
