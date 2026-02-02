da915fd7050abf3b26b6ab8a8153eae78ecedd20
Paolo Davini <p.davini@isac.cnr.it>
2026-02-02 11:32:41 +0100

    improve logging for mismatch in vertical coordinate

--- smmregrid/regrid.py
+++ smmregrid/regrid.py
@@ -363,8 +363,14 @@ class Regridder(object):
         data3d_list = []
         # create pandas object for fast level index retrieval
         mask_index = weights.coords[mask_dim].to_index()
+        print(mask_index)
         for idx, lev in enumerate(source_data.coords[mask_dim].values):
             # get the index of the level for weights selection (widx, which might be different from idx)
+            if lev not in mask_index:
+                raise ValueError(
+                    f"{lev} not found in mask_dim {mask_index.name}. "
+                    f"Available levels: {list(mask_index.values)}"
+                )
             widx = mask_index.get_loc(lev)
             self.loggy.debug('Processing vertical level %s - level_index %s', lev, widx)
             # use isel since it faster
